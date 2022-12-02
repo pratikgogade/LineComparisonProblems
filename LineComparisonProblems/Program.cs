4a3a02f1b4a7e6edba984f414e956299d59309d5
unknown <Pratik@Example.com>
2022-12-03 01:24:55 +0530

    [Pratik]Add:Comparison Of Lines

--- LineComparisonProblems/Program.cs
+++ LineComparisonProblems/Program.cs
@@ -7,20 +7,45 @@ namespace LineComparisonProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter x1:");
-            int x1 = Convert.ToInt32(Console.ReadLine());
+            double L1 = 0, L2 = 0;
+            for (int i = 0; i <= 1; i++)
+            {
+                Console.WriteLine("Enter x1:");
+                int x1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter y1:");
-            int y1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter y1:");
+                int y1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter x2:");
-            int x2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter x2:");
+                int x2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter y2:");
-            int y2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter y2:");
+                int y2 = Convert.ToInt32(Console.ReadLine());
 
-            double Length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            Console.WriteLine("Length: " + Length);
+                double Length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+                if (i == 0)
+                {
+                    Console.WriteLine("Length Of Line1: " + Length);
+                    L1 = Length;
+                }
+                else
+                {
+                    Console.WriteLine("Length Of Line2: " + Length);
+                    L2 = Length;
+                }
+            }
+            if (L1 == L2)
+            {
+                Console.WriteLine("Lines Are Equal");
+            }
+            else if (L1 > L2)
+            {
+                Console.WriteLine("Line1 is greater than Line2");
+            }
+            else
+            {
+                Console.WriteLine("Line2 is greater than Line1");
+            }
         }
     }
 }
